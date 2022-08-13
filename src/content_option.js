245cfca78e9419e4314e305a01cebe60c3d0980a
Mirafzal1307 <usmonjonovmirafzal0713@gmail.com>
2022-08-13 10:53:31 +0500

    My Portfolio is finished successfully

---
+++ src/content_option.js
@@ -0,0 +1,192 @@
+const logotext = "U.M.M";
+const meta = {
+    title: "U.M.Muzaffarovich",
+    description: "I’m Usmonjonov Mirafzal front-end devloper,currently working in iTech",
+};
+
+const introdata = {
+    title: "I’m Mirafzal",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop web apps",
+    },
+    description: "I am a front-end developer, currently working in iTech",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A little bit about my self",
+    aboutme: "I’m a nice fun and friendly person, I’m honest and punctual, I work well in a team but also on my own as I like to set myself goals which I will achieve, I have good listening and communication skills. I have a creative mind and am always up for new challenges. I am well organized and always plan ahead to make sure I manage my time well",
+};
+const worktimeline = [{
+        jobtitle: "Front-end web developer",
+        where: "iTech",
+        date: "until 2020 - now",
+    },
+];
+
+const skills = [{
+        name: "Typesctipt",
+        value: 80,
+    },
+    {
+        name: "Redux",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 85,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+    {
+        name: "Git",
+        value: 85,
+    },
+    {
+        name: "HTML",
+        value: 85,
+    },
+    {
+        name: "CSS",
+        value: 85,
+    },
+    {
+        name: "SASS",
+        value: 80,
+    },
+    {
+        name: "Bootstrap",
+        value: 95,
+    },
+    {
+        name: "Photoshop",
+        value: 75,
+    },
+    {
+        name: "Material UI",
+        value: 80,
+    },
+    {
+        name: "Tailwind CSS",
+        value: 80,
+    },
+    {
+        name: "Vue",
+        value: 75,
+    },
+    {
+        name: "Vuex",
+        value: 70,
+    },
+    {
+        name: "Nuxt JS",
+        value: 70,
+    },
+];
+
+const services = [{
+        title: "Web Development",
+        description: "I develop web apps using React and Redux",
+    }
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "usmonjonovmirafzal0713@gmail.com",
+    YOUR_FONE: "(998)93 105-71-34",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Mirafzal1307",
+    facebook: "https://www.facebook.com/profile.php?id=100074563819796",
+    linkedin: "https://www.linkedin.com/in/mirafzal-usmonjonov-752138227/",
+    twitter: "https://twitter.com",
+    instagram: "https://www.instagram.com/u.m.muzaffarovich/",
+    telegram: "https://t.me/UMMUZAFFAROVICH",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
